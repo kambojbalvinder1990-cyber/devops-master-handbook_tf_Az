2407d7e6c758e7409a82b1ef79ad1a8d04d2e4d7
devops-master-handbook_tf_Az <kambojbalvinder1990@gmail.com>
2026-08-11 20:49:00 +0530

    backend updated

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -6,11 +6,12 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "git_rg"
-    storage_account_name = "git12345"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
+  use_azuread_auth     = true
+  resource_group_name  = "git_rg"
+  storage_account_name = "git12345"
+  container_name       = "tfstate"
+  key                  = "prod.terraform.tfstate"
+}
 }
 provider "azurerm" {
   features {}
