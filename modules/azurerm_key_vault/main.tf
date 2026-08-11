5e6f943b74d0a5b366275c006a1da28261de46e2
devops-master-handbook_tf_Az <kambojbalvinder1990@gmail.com>
2026-08-11 23:40:05 +0530

    keyvault authentication

--- modules/azurerm_key_vault/main.tf
+++ modules/azurerm_key_vault/main.tf
@@ -1,10 +1,11 @@
 resource "azurerm_key_vault" "kv" {
-  for_each            = var.key_vaults
-  name                = each.value.name
-  location            = each.value.location
-  resource_group_name = each.value.rg_name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+  for_each                   = var.key_vaults
+  name                       = each.value.name
+  location                   = each.value.location
+  resource_group_name        = each.value.rg_name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  rbac_authorization_enabled = false
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
