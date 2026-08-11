14f4235e7a9262f53a0f464b2b2be53ade9b9bcd
devops-master-handbook_tf_Az <kambojbalvinder1990@gmail.com>
2026-08-11 15:03:06 +0530

    added backend block

--- environments/prod/provider.tf
+++ environments/prod/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "git_rg"
+    storage_account_name = "git12345"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features {}
